e2c4d08d2912c3c6456726f9e0fdea743a2a549a
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-09-25 19:59:41 +0800

    Create Purchasing Information and Receipts for Lovely Loveseats.py

---
+++ Python/Exercises/Purchasing Information and Receipts for Lovely Loveseats.py
@@ -0,0 +1,33 @@
+# PYTHON FUNCTIONS AND LOGIC
+lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+
+lovely_loveseat_price = 254.00
+
+stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+
+stylish_settee_price = 185.50
+
+luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+
+luxurious_lamp_price = 52.15
+
+sales_tax = .088
+
+customer_one_total = 0
+
+customer_one_itemization = ""
+
+customer_one_total += lovely_loveseat_price
+
+customer_one_itemization += lovely_loveseat_description
+customer_one_total += luxurious_lamp_price
+
+customer_one_itemization += luxurious_lamp_description
+
+customer_one_tax = customer_one_total * sales_tax
+
+customer_one_total += sales_tax
+
+print("Customer One Items:\n %s \n" %customer_one_itemization)
+
+print("Customer One Total: %d \n" %customer_one_total)
