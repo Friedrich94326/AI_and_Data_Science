12bfb8e3361b71dd6cbd225f3df91b642e74c609
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-03 20:06:39 +0800

    Update Aggregates_Statistics.py

--- Python/Pandas/Aggregates_Statistics.py
+++ Python/Pandas/Aggregates_Statistics.py
@@ -29,5 +29,45 @@ cheap_shoes = orders.groupby('shoe_color').price.apply(lambda x: np.percentile(x
 print(cheap_shoes)
 
 # group by more than one column by passing a list of column names into the groupby method
+shoe_counts = orders.groupby(['shoe_type', 'shoe_color']).id.count().reset_index()
+print(shoe_counts)
+print(type(shoe_counts))
+
+# Create a pivot table
+shoe_counts = orders.groupby(['shoe_type', 'shoe_color']).id.count().reset_index()
+shoe_counts_pivot = shoe_counts.pivot(
+    columns = 'shoe_color',
+    index = 'shoe_type',
+    values = 'id'  
+).reset_index()  # a pivot table is a dataframe, providing a great way to compare data across two dimensions
+print(shoe_counts_pivot)
+
+
+
+
+# the following is a review
+user_visits = pd.read_csv('page_visits.csv')
+
+print(user_visits.head())
+
+# calculate how many visits came from each of the different sources
+click_source = user_visits.groupby('utm_source').id.count().reset_index()
+print(click_source)
+
+click_source_by_month = user_visits.groupby(['utm_source', 'month']).id.count().reset_index()
+clikc_source_by_month_pivot = click_source_by_month.pivot(
+    columns = 'month',
+    index = 'utm_source',
+    values = 'id').reset_index()
+
+
+
+
+
+
+
+
+
+
 
 
