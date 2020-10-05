7ee1ca4f8c5a4c8614d5403e261fb3ff5c22fad0
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-05 11:27:49 +0800

    Add files via upload

---
+++ Python/Projects/Data Visualisation/roller_coaster_starting/script.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load rankings data here:
+
+
+
+
+
+# write function to plot rankings over time for 1 roller coaster here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
+
+# write function to plot rankings over time for 2 roller coasters here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
+
+# write function to plot top n rankings over time here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
+
+# load roller coaster data here:
+
+
+
+# write function to plot histogram of column values here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
+
+# write function to plot inversions by coaster at a park here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
+
+# write function to plot pie chart of operating status here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
+
+# write function to create scatter plot of any two numeric columns here:
+
+
+
+
+
+
+
+
+
+
+plt.clf()
