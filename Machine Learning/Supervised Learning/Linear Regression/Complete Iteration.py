67e78a0fff887ecb927da20f327d1085ff45f1d8
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-07 22:27:04 +0800

    Create Complete Iteration.py

---
+++ Machine Learning/Supervised Learning/Linear Regression/Complete Iteration.py
@@ -0,0 +1,54 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+
+def get_gradient_at_b(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+    x_val = x[i]
+    y_val = y[i]
+    diff += (y_val - ((m * x_val) + b))
+  b_gradient = -(2/N) * diff  
+  return b_gradient
+
+def get_gradient_at_m(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+      x_val = x[i]
+      y_val = y[i]
+      diff += x_val * (y_val - ((m * x_val) + b))
+  m_gradient = -(2/N) * diff  
+  return m_gradient
+
+#Your step_gradient function here
+def step_gradient(b_current, m_current, x, y, learning_rate):
+    b_gradient = get_gradient_at_b(x, y, b_current, m_current)
+    m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+    b = b_current - (learning_rate * b_gradient)
+    m = m_current - (learning_rate * m_gradient)
+    return [b, m]
+  
+#Your gradient_descent function here:
+def gradient_descent(x, y, learning_rate, num_iterations):
+  # initial guesses
+  [b, m] = [0, 0]
+  for i in range(num_iterations):
+    b, m = step_gradient(b, m, x, y, learning_rate)
+  return [b, m]
+
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+revenue = [52, 74, 79, 95, 115, 110, 129, 126, 147, 146, 156, 184]
+
+#Uncomment the line below to run your gradient_descent function
+b, m = gradient_descent(months, revenue, 0.01, 1000)
+
+#Uncomment the lines below to see the line you've settled upon!
+y = [m*x + b for x in months]
+
+plt.plot(months, revenue, "o")
+plt.plot(months, y)
+
+plt.show()
+
+
