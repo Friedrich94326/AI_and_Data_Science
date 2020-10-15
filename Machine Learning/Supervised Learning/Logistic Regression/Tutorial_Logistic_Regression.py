e1b2119deffd8e7e63d150b2b98a3fd02410c7ce
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-15 17:36:02 +0800

    Update Tutorial_Logistic_Regression.py

--- Machine Learning/Supervised Learning/Logistic Regression/Tutorial_Logistic_Regression.py
+++ Machine Learning/Supervised Learning/Logistic Regression/Tutorial_Logistic_Regression.py
@@ -83,4 +83,9 @@ print(calculated_log_odds)
 # Sigmoid Function
 from exam import calculated_log_odds
 
+def sigmoid(z):
+  denominator = 1 + np.exp(-z)
+  return 1 / denominator
 
+probabilities = sigmoid(calculated_log_odds)
+print(probabilities)
