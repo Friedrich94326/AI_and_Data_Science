6bd5ea7320f6e1f51bd51fe26de48195427d0b59
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-26 20:46:02 +0800

    Create Twitter Classification: Part I.py

---
+++ Machine Learning/Supervised Learning/Projects/Twitter Classification Cumulative Project/Twitter Classification: Part I.py
@@ -0,0 +1,4 @@
+"""
+In the first part, you will make a system that predicts whether or not a tweet will go viral by using a K-Nearest Neighbor classifier.
+What features of a tweet do you think are the most important in determining its virality? 
+"""
