a3f5f52b8ecf1ba21e534f6f8d01e4230d640754
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-26 20:48:32 +0800

    Create Twitter Classification: Part II.py

---
+++ Machine Learning/Supervised Learning/Projects/Twitter Classification Cumulative Project/Twitter Classification: Part II.py
@@ -0,0 +1,6 @@
+"""
+In the second part of this project, you’ll test the power of Naive Bayes classifiers
+by creating a system that predicts whether a tweet was sent from New York City, London, or Paris.
+You will investigate how language is used differently in these three cities.
+"""
+
