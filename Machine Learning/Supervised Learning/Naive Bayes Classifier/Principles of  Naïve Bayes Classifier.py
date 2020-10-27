4165ed6b61785abec2e5db89d2b00b7cb017f798
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-10-27 19:41:46 +0800

    Create Principles of  Naïve Bayes Classifier.py

---
+++ Machine Learning/Supervised Learning/Naive Bayes Classifier/Principles of  Naïve Bayes Classifier.py
@@ -0,0 +1,2 @@
+# A Naive Bayes classifier is a supervised machine learning algorithm that leverages Bayes’ Theorem to make predictions and classifications. 
+# Naive Bayes classifiers are often used for text classification
