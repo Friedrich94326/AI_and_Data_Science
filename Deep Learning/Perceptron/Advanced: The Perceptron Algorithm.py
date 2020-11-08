6564e5a00ac0b034a1b8303e32dc7d6de78e3e25
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-11-08 16:39:40 +0800

    Update and rename Deep Learning/Advanced: The Perceptron Algorithm.py to Deep Learning/Perceptron/Advanced: The Perceptron Algorithm.py

---
+++ Deep Learning/Perceptron/Advanced: The Perceptron Algorithm.py
@@ -0,0 +1,142 @@
+""" the perceptron can be trained to produce correct outputs by tweaking the regular weights """
+
+
+class Perceptron:
+  def __init__(self, num_inputs = 2, weights = [1, 1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
+    
+  def weighted_sum(self, inputs):
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += self.weights[i] * inputs[i]
+    return weighted_sum
+  
+  def activation(self, weighted_sum):
+    if weighted_sum >= 0:
+      return 1
+    else:
+      return -1
+    
+  def training(self, training_set):
+    foundLine = False
+    while not foundLine:
+      total_error = 0
+      for inputs in training_set:
+        prediction = self.activation(self.weighted_sum(inputs))
+        actual = training_set[inputs]
+        error = actual - prediction
+        total_error += abs(error)
+        for i in range(self.num_inputs):
+          self.weights[i] += error*inputs[i]
+      if total_error == 0:
+        foundLine = True
+      
+cool_perceptron = Perceptron()
+small_training_set = {(0,3):1, (3,0):-1, (0,-3):-1, (-3,0):1}
+cool_perceptron.training(small_training_set)
+print(cool_perceptron.weights)
+
+
+# The Bias Weight
+
+class Perceptron:
+  def __init__(self, num_inputs = 3, weights = [1,1,1]):  # Slight change: Add a 1 to the set of inputs (now there are 3 inputs instead of 2)
+    self.num_inputs = num_inputs                                          #Add a bias weight to the list of weights (now there are 3 weights instead of 2)
+    self.weights = weights
+    
+  def weighted_sum(self, inputs):
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += self.weights[i] * inputs[i]
+    return weighted_sum
+  
+  def activation(self, weighted_sum):
+    if weighted_sum >= 0:
+      return 1
+    else:
+      return -1
+    
+  def training(self, training_set):
+    foundLine = False
+    while not foundLine:
+      total_error = 0
+      for inputs in training_set:
+        prediction = self.activation(self.weighted_sum(inputs))
+        actual = training_set[inputs]
+        error = actual - prediction
+        total_error += abs(error)
+        for i in range(self.num_inputs):
+          self.weights[i] += error * inputs[i]
+      if total_error == 0:
+        foundLine = True
+      
+cool_perceptron = Perceptron()
+
+# Representing a Line
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+from perceptron import Perceptron, lines
+
+def generate_training_set(num_points):
+	x_coordinates = [random.randint(0, 50) for i in range(num_points)]
+	y_coordinates = [random.randint(0, 50) for i in range(num_points)]
+	training_set = dict()
+	for x, y in zip(x_coordinates, y_coordinates):
+		if x <= 45-y:
+			training_set[(x,y,1)] = 1
+		elif x > 45-y:
+			training_set[(x,y,1)] = -1
+	return training_set
+
+training_set = generate_training_set(30)
+
+x_plus = []
+y_plus = []
+x_minus = []
+y_minus = []
+
+for data in training_set:
+	if training_set[data] == 1:
+		x_plus.append(data[0])
+		y_plus.append(data[1])
+	elif training_set[data] == -1:
+		x_minus.append(data[0])
+		y_minus.append(data[1])
+
+perceptron = Perceptron()
+perceptron.training(training_set)
+
+fig = plt.figure()
+ax = plt.axes(xlim=(-25, 75), ylim=(-25, 75))
+line, = ax.plot([], [], lw=2)
+
+fig.patch.set_facecolor('#ffc107')
+
+plt.scatter(x_plus, y_plus, marker = '+', c = 'green', s = 128, linewidth = 2)
+plt.scatter(x_minus, y_minus, marker = '_', c = 'red', s = 128, linewidth = 2)
+
+plt.title('Iteration: 0')
+
+
+def animate(i):
+    print(i)
+    line.set_xdata(lines[i][0])  # update the data
+    line.set_ydata(lines[i][1])  # update the data
+    return line,
+
+def init():
+    line.set_data([], [])
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, frames=1, init_func=init, interval=50, blit=True, repeat=False)
+
+plt.show()
+
+
+""" A single perceptron with only two inputs wouldn’t work for such a scenario because it cannot represent a non-linear decision boundary. 
+By increasing the number of features and perceptrons, we can give rise to the Multilayer Perceptrons, also known as Neural Networks, which 
+can solve much more complicated problems.
+"""
