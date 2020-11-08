4b1f28acf69dfa9e477ad4eb8392cccffa1b30e0
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-11-09 01:25:54 +0800

    Update Implementing Neural Networks.py

--- Deep Learning/Neural Networks/Implementing Neural Networks.py
+++ Deep Learning/Neural Networks/Implementing Neural Networks.py
@@ -22,4 +22,47 @@ print(labels.shape)
 # summary statistics of the 'labels' series
 print(labels.describe())
 
+""" Data preprocessing: one-hot encoding and standardization """
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+
+# one-hot encoding for categorical variables
+features = pd.get_dummies(features) 
+# split the data into training and test data
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) 
+ 
+# normalize the numeric columns using ColumnTransformer
+ct = ColumnTransformer([('normalize', Normalizer(), ['age', 'bmi', 'children'])], remainder='passthrough')
+# fit the normalizer to the training data and convert from numpy arrays to pandas frame
+features_train_norm = ct.fit_transform(features_train) 
+# applied the trained normalizer on the test data and convert from numpy arrays to pandas frame
+features_test_norm = ct.transform(features_test) 
+
+# ColumnTransformer returns numpy arrays. Convert the features to dataframes
+features_train_norm = pd.DataFrame(features_train_norm, columns = features_train.columns)
+features_test_norm = pd.DataFrame(features_test_norm, columns = features_test.columns)
+
+# -----> your code here below
+my_ct = ColumnTransformer([('scale', StandardScaler(), ['age', 'bmi', 'children'])], remainder = 'passthrough')
+
+features_train_scale = my_ct.fit_transform(features_train)
+features_test_scale = my_ct.transform(features_test)
+print(type(features_train_scale))
+
+# Transform the features_train_scale NumPy array back to a DataFrame
+features_train_scale = pd.DataFrame(features_train_scale,columns = features_train.columns)
+
+# Transform the features_test_scale NumPy array back to DataFrame
+features_test_scale = pd.DataFrame(features_test_scale,columns = features_test.columns)
+
+# Print the statistics summary
+print(features_train_scale.describe())
+print(features_test_scale.describe())
+
+
+
+
 
