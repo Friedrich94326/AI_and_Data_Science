9fc3802d24ec4377c4ca4f388d073bbaae49e9a1
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-11-08 18:31:27 +0800

    Create Implementing Neural Networks.py

---
+++ Deep Learning/Neural Networks/Implementing Neural Networks.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#load the dataset
+dataset = pd.read_csv('insurance.csv') 
+#choose first 7 columns as features
+features = dataset.iloc[:,0:6] 
+#choose the final column for prediction
+labels = dataset.iloc[:,-1] 
+
+#print the number of features in the dataset
+print("Number of features: ", features.shape[1]) 
+#print the number of samples in the dataset
+print("Number of samples: ", features.shape[0]) 
+#see useful summary statistics for numeric features
+print(features.describe()) 
+
+# number of samples of the 'labels' series
+print(labels.shape)
+# summary statistics of the 'labels' series
+print(labels.describe())
