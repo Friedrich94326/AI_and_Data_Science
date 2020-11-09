89ffefde046ccc0d2214bd1110e0139aba8cb419
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-11-09 13:39:00 +0800

    Update and rename Building Neural Network Models with TensorFlow.py to Building Neural Network Models in TensorFlow with Keras.py

--- Deep Learning/Neural Networks/Building Neural Network Models in TensorFlow with Keras.py
+++ Deep Learning/Neural Networks/Building Neural Network Models in TensorFlow with Keras.py
@@ -17,18 +17,13 @@ features = dataset.iloc[:,0:6] #choose first 7 columns as features
 labels = dataset.iloc[:,-1] #choose the final column for prediction
 
 features = pd.get_dummies(features) #one-hot encoding for categorical variables
-features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) #split the data into training and test data
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.33, random_state = 42) #split the data into training and test data
  
+
 #standardize
 ct = ColumnTransformer([('standardize', StandardScaler(), ['age', 'bmi', 'children'])], remainder='passthrough')
 features_train = ct.fit_transform(features_train)
 features_test = ct.transform(features_test)
-
-#invoke the function for our model design
-model = design_model(features_train)
-
-#print the layers
-print(model.layers)
 
 
 """ Neural Network Model- Layers: Layers are the building blocks of neural networks and can contain 1 or more neurons. 
@@ -66,23 +61,64 @@ def design_model(features):
   return model
 
 
-dataset = pd.read_csv('insurance.csv') #load the dataset
-features = dataset.iloc[:,0:6] #choose first 7 columns as features
-labels = dataset.iloc[:,-1] #choose the final column for prediction
 
-features = pd.get_dummies(features) #one-hot encoding for categorical variables
-features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) #split the data into training and test data
- 
-#standardize
-ct = ColumnTransformer([('standardize', StandardScaler(), ['age', 'bmi', 'children'])], remainder='passthrough')
-features_train = ct.fit_transform(features_train)
-features_test = ct.transform(features_test)
+""" Neural Network Model: Output Layer """
+""" The output layer shape depends on your task. In the case of regression, we need one output for each sample. """
+from tensorflow.keras.layers import Dense
+
+def design_model(features):
+  model = Sequential(name = "my_first_model")
+  num_features = features.shape[1]
+  input = InputLayer(input_shape = (num_features,))
+  model.add(input) #add the input layer
+  #add an output layer to the model
+  model.add(Dense(1))
+  return model
+
+
+""" Neural Network Model- Hidden Layers: To capture more complex or non-linear interactions
+among the inputs and outputs neural networks, we’ll need to incorporate hidden layers. """
+
+
+def design_model(features):
+  model = Sequential(name = "my_first_model")
+  input = InputLayer(input_shape = (features.shape[1],))
+  #add the input layer
+  model.add(input) 
+  #add a new hidden layer to the model
+  model.add(Dense(128, activation = 'relu'))
+
+  #adding an output layer to our model
+  model.add(Dense(1)) 
+  return model
+
+
+
+""" Optimisers: The neural network effectively adjusts its weights or parameters in order to reach the best performance """
+from tensorflow.keras.optimizers import Adam
+
+def design_model(features):
+  model = Sequential(name = "my_first_model")
+  input = InputLayer(input_shape=(features.shape[1],))
+   #add an input layer
+  model.add(input)
+  #add a hidden layer with 128 neurons
+  model.add(Dense(128, activation='relu')) 
+  #add an output layer
+  model.add(Dense(1)) 
+
+  #create an Adam optimiser and then compile the model 
+  opt = Adam(learning_rate = 0.01)
+  #loss function: measure of learning success, "mse": mean-squared error
+  #how far we're off from the true values in the units we are predicting: the progress of "mae": mean absolute error
+  model.compile(loss = 'mse', metrics = ['mae'], optimizer = opt)
+
+  return model
+
 
 #invoke the function for our model design
 model = design_model(features_train)
 
-#print a useful summary of a model instance
+#print the model summary here
 print(model.summary())
-
-
 
