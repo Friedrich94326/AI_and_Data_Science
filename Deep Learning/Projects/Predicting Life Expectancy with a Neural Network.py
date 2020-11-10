cd8687e5aaab6da153683b16f84b0fcc843568ef
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-11-10 15:13:16 +0800

    Update Predicting Life Expectancy with a Neural Network.py

--- Deep Learning/Projects/Predicting Life Expectancy with a Neural Network.py
+++ Deep Learning/Projects/Predicting Life Expectancy with a Neural Network.py
@@ -12,7 +12,18 @@ the life expectancy of countries using this dataset.
 
 
 import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.compose import ColumnTransformer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
+
+""" Data Loading and Observing """
 dataset = pd.read_csv('life_expectancy.csv')
 
 print(dataset.head())
@@ -25,8 +36,52 @@ dataset = dataset.drop(['Country'], axis = 1)
 print(dataset.head())
 print(dataset.columns)
 
+""" Data Preprocessing """
+# convert categorical columns into numerical ones
+dataset = pd.get_dummies(dataset)
+
 # labels: the last column of data
 labels = dataset.iloc[:, -1]
 
 # features: the remaining columns of data
-features = dataset.iloc[:, ]
+features = dataset.iloc[:, 0:-1]
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.20, random_state = 100)
+
+# normalise the numerical features
+numerical_features = features.select_dtypes(include = ['float64', 'int64'])
+numerical_columns = numerical_features.columns
+ct = ColumnTransformer([('standardize', StandardScaler(), numerical_columns)], remainder = 'passthrough')
+
+features_train_scaled = ct.fit_transform(features_train)
+features_test_scaled = ct.fit_transform(features_test)
+
+
+""" Building the Model """
+num_features = features.shape[1]
+my_model = Sequential()
+
+# create an input layer and add it to the model
+input = InputLayer(input_shape = (num_features, ))
+my_model.add(input)
+
+# create a hidden layer and add it to the model
+my_model.add(Dense(64, activation = 'relu'))
+
+# create an output layer and add it to the model
+my_model.add(Dense(1)) # for regression prediction
+
+print(my_model.summary())
+
+""" Intialising the Optimiser and Compiling the Model """
+opt_adam = Adam(learning_rate = 0.01)
+my_model.compile(loss = 'mse',  metrics = ['mae'], optimizer = opt_adam)
+
+
+""" Train and Evaluate the model """
+my_model.fit(features_train_scaled, labels_train, epochs = 40, batch_size = 1, verbose = 1)
+
+res_mse, res_mae = my_model.evaluate(feautres_test_scaled, labels_test, verbose = 0)
+
+print("MSE test: ", res_mse)
+print("MAE test: ", res_mae)
