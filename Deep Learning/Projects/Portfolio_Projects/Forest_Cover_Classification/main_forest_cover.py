0b60cff9430c6d5059bc18bd39df7482bc3b2e37
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-11-18 18:02:45 +0800

    Create main_forest_cover.py

---
+++ Deep Learning/Projects/Portfolio_Projects/Forest_Cover_Classification/main_forest_cover.py
@@ -0,0 +1,24 @@
+""" About this Project:
+To use deep learning to predict forest cover type (the most common kind of tree cover) based only on cartographic variables.
+The actual forest cover type for a given 30 x 30 meter cell was determined from US Forest Service (USFS) Region 2 Resource Information System data. 
+
+The covertypes are the following:
+
+  Spruce/Fir
+  Lodgepole Pine
+  Ponderosa Pine  
+  Cottonwood/Willow
+  Aspen
+  Douglas-fir
+  Krummholz
+  
+Project Objectives:
+  1. Develop one or more classifiers for this multi-class classification problem.
+  2. Use TensorFlow with Keras to build your classifier(s).
+  3. Use your knowledge of hyperparameter tuning to improve the performance of your model(s).
+  4. Test and analyze performance.
+  5. Create clean and modular code.
+"""
+
+
+
