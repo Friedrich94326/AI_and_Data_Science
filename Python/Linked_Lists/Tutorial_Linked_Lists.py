c1aa5218a40b58ab33e56d2ca2ed65aa8c5f6fc8
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2021-01-03 18:48:30 +0800

    Update Tutorial_Linked_Lists.py

--- Python/Linked_Lists/Tutorial_Linked_Lists.py
+++ Python/Linked_Lists/Tutorial_Linked_Lists.py
@@ -24,6 +24,28 @@ print(my_node.get_value())
 class LinkedList:
   def __init__(self, value = None):
     self.head_node = Node(value = value)
-  
+    
   def get_head_node(self):
     return self.head_node
+
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+
+  def stringify_list(self):
+    string_list = ''
+    current_node = self.get_head_node()
+    while (current_node):
+      if (current_node.get_value() is not None):
+        string_list += str(current_node.get_value()) + '\n'
+        current_node = current_node.get_next_node()
+    
+    return string_list
+
+# Test your code by uncommenting the statements below - did your list print to the terminal?
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())
