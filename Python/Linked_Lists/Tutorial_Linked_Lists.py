98f0b1142c0f26107e3131ded5f95752fbde6481
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2021-01-03 19:14:43 +0800

    Update Tutorial_Linked_Lists.py

--- Python/Linked_Lists/Tutorial_Linked_Lists.py
+++ Python/Linked_Lists/Tutorial_Linked_Lists.py
@@ -43,9 +43,27 @@ class LinkedList:
     
     return string_list
 
+
+
+def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+
 # Test your code by uncommenting the statements below - did your list print to the terminal?
 ll = LinkedList(5)
 ll.insert_beginning(70)
 ll.insert_beginning(5675)
 ll.insert_beginning(90)
+print(ll.stringify_list())
+
+ll.remove_node(90)
 print(ll.stringify_list())
